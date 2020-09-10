fe711fd6f5566c66c212290b5ae19ab4890b6bd6
MatsyshynAnton <anton.matsyshyn@outlook.com>
2020-09-10 21:35:11 +0300

    Add Fibonacci calculating logic

--- Fibonacci/Fibonacci/Program.cs
+++ Fibonacci/Fibonacci/Program.cs
@@ -6,7 +6,22 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Value of third Fibonacci number: {GetFibonacciWrapper(3)}");
+            Console.WriteLine($"Value of fifth Fibonacci number: {GetFibonacciWrapper(5)}");
+            Console.WriteLine($"Value of ninth Fibonacci number: {GetFibonacciWrapper(9)}");
+        }
+
+        static int GetFibonacciWrapper(int seqNumber)
+        {
+            seqNumber -= 2;
+            return GetFibonacci(seqNumber);
+        }
+        static int GetFibonacci(int seqNumber, int first = 1, int second = 1)
+        {
+            if (seqNumber <= 0)
+                return first + second;
+
+            return GetFibonacci(--seqNumber, second, first + second);
         }
     }
 }
